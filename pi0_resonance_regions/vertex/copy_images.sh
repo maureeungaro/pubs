f6d4e05818c4ad87d28465445a28cadfc61e18ed
maureeungaro <maureeungaro@gmail.com>
2023-01-31 14:32:43 -0500

    added vertex section

---
+++ pi0_resonance_regions/vertex/copy_images.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+mkdir -p img
+
+orig_dir=/opt/projects/mauriplots/ppid/
+orig_dir=/opt/projects/analysis/vertex # remove after copying to mauriplots
+
+exit 0
+
+ppid_imgs=()
+
+ppid_imgs_slices=()
+
+for cc in $ppid_imgs; do
+    echo $cc
+    cp $orig_dir/img/$cc.png img
+done
+
+for cc in $ppid_imgs_slices; do
+    echo $cc
+    cp $orig_dir/img_slices/$cc.png img
+done
