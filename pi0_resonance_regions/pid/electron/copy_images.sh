ce19ce67bb4ab6bdb2a19471e5173661013b77f2
maureeungaro <maureeungaro@gmail.com>
2023-01-31 13:18:53 -0500

    added proton id tex files

--- pi0_resonance_regions/pid/electron/copy_images.sh
+++ pi0_resonance_regions/pid/electron/copy_images.sh
@@ -6,8 +6,7 @@ orig_dir=/opt/projects/mauriplots/epid/
 orig_dir=/opt/projects/analysis/pid/electron # remove after copying to mauriplots
 
 
-
-cc_match_files=(cut-01-cc-theta-match-pars_sector-1
+epid_imgs=(cut-01-cc-theta-match-pars_sector-1
                 cut-01-cc-theta-match-compare_sector-1
                 cut-02-cc-phi-match_sector-1
                 cut-03-cc-time-match-compare_sector-1
@@ -25,7 +24,7 @@ cc_match_files=(cut-01-cc-theta-match-pars_sector-1
                 cut-12-nphe_sector-all
                 epidsummary)
 
-cc_match_files_slices=(slice-06_cut-01-cc-theta-slice_sector-1
+epid_imgs_slices=(slice-06_cut-01-cc-theta-slice_sector-1
                        slice-14_cut-01-cc-theta-slice_sector-1
                        slice-03_cut-03-cc-time-match_sector-1
                        slice-08_cut-03-cc-time-match_sector-1
@@ -34,12 +33,12 @@ cc_match_files_slices=(slice-06_cut-01-cc-theta-slice_sector-1
                        slice-04_cut-05-sampling-f_sector-1
                        slice-08_cut-05-sampling-f_sector-1)
 
-for cc in $cc_match_files; do
+for cc in $epid_imgs; do
     echo $cc
     cp $orig_dir/img/$cc.png img
 done
 
-for cc in $cc_match_files_slices; do
+for cc in $epid_imgs_slices; do
     echo $cc
     cp $orig_dir/img_slices/$cc.png img
 done
