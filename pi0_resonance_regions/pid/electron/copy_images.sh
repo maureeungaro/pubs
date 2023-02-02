415661f533f68237b716758594dc6b13cdd959dd
maureeungaro <maureeungaro@gmail.com>
2023-02-02 08:42:22 -0500

    proton id section

--- pi0_resonance_regions/pid/electron/copy_images.sh
+++ pi0_resonance_regions/pid/electron/copy_images.sh
@@ -5,40 +5,43 @@ mkdir -p img
 orig_dir=/opt/projects/mauriplots/epid/
 orig_dir=/opt/projects/analysis/pid/electron # remove after copying to mauriplots
 
+epid_imgs=(
+  cut-01-cc-theta-match-pars_sector-1
+  cut-01-cc-theta-match-compare_sector-1
+  cut-02-cc-phi-match_sector-1
+  cut-03-cc-time-match-compare_sector-1
+  cut-04-ec-threshold_sector-1
+  cut-05-sampling-f_sector-1
+  cut-05-sampling-f-compare_sector-1
+  cut-05-sampling-f_sector-all
+  cut-06-ECU_sector-all
+  cut-07-ECV_sector-all
+  cut-08-ECW_sector-all
+  cut-09-UVW_sector-all
+  cut-10-mip_sector-1
+  cut-10-mip_sector-all
+  cut-11-shower-shape_sector-1
+  cut-12-nphe_sector-all
+  epidsummary
+)
 
-epid_imgs=(cut-01-cc-theta-match-pars_sector-1
-                cut-01-cc-theta-match-compare_sector-1
-                cut-02-cc-phi-match_sector-1
-                cut-03-cc-time-match-compare_sector-1
-                cut-04-ec-threshold_sector-1
-                cut-05-sampling-f_sector-1
-                cut-05-sampling-f-compare_sector-1
-                cut-05-sampling-f_sector-all
-                cut-06-ECU_sector-all
-                cut-07-ECV_sector-all
-                cut-08-ECW_sector-all
-                cut-09-UVW_sector-all
-                cut-10-mip_sector-1
-                cut-10-mip_sector-all
-                cut-11-shower-shape_sector-1
-                cut-12-nphe_sector-all
-                epidsummary)
-
-epid_imgs_slices=(slice-06_cut-01-cc-theta-slice_sector-1
-                       slice-14_cut-01-cc-theta-slice_sector-1
-                       slice-03_cut-03-cc-time-match_sector-1
-                       slice-08_cut-03-cc-time-match_sector-1
-                       slice-12_cut-03-cc-time-match_sector-1
-                       slice-15_cut-03-cc-time-match_sector-1
-                       slice-04_cut-05-sampling-f_sector-1
-                       slice-08_cut-05-sampling-f_sector-1)
+epid_imgs_slices=(
+  slice-06_cut-01-cc-theta-slice_sector-1
+  slice-14_cut-01-cc-theta-slice_sector-1
+  slice-03_cut-03-cc-time-match_sector-1
+  slice-08_cut-03-cc-time-match_sector-1
+  slice-12_cut-03-cc-time-match_sector-1
+  slice-15_cut-03-cc-time-match_sector-1
+  slice-04_cut-05-sampling-f_sector-1
+  slice-08_cut-05-sampling-f_sector-1
+)
 
 for cc in $epid_imgs; do
-    echo $cc
-    cp $orig_dir/img/$cc.png img
+  echo $cc
+  cp $orig_dir/img/$cc.png img
 done
 
 for cc in $epid_imgs_slices; do
-    echo $cc
-    cp $orig_dir/img_slices/$cc.png img
+  echo $cc
+  cp $orig_dir/img_slices/$cc.png img
 done
--- pi0_resonance_regions/pid/proton/copy_images.sh
+++ pi0_resonance_regions/pid/proton/copy_images.sh
@@ -5,18 +5,23 @@ mkdir -p img
 orig_dir=/opt/projects/mauriplots/ppid/
 orig_dir=/opt/projects/analysis/pid/proton # remove after copying to mauriplots
 
-exit 0
+ppid_imgs=(
+  dist-dtfit_sector-1
+  dist-dtfit_sector-all
+  dist-massandbeta_sector-1
+)
 
-ppid_imgs=()
-
-ppid_imgs_slices=()
+ppid_imgs_slices=(
+  slice-2_sector-1
+  slice-4_sector-1
+)
 
 for cc in $ppid_imgs; do
-    echo $cc
-    cp $orig_dir/img/$cc.png img
+  echo $cc
+  cp $orig_dir/img/$cc.png img
 done
 
 for cc in $ppid_imgs_slices; do
-    echo $cc
-    cp $orig_dir/img_slices/$cc.png img
+  echo $cc
+  cp $orig_dir/img_slices/$cc.png img
 done
