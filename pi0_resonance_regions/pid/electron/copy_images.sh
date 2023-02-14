6c3a175c49711f7164bca54dc05efb1994a1f908
maureeungaro <maureeungaro@gmail.com>
2023-02-14 15:45:34 -0500

    added fiducial electron tex files

--- pi0_resonance_regions/pid/electron/copy_images.sh
+++ pi0_resonance_regions/pid/electron/copy_images.sh
@@ -11,34 +11,11 @@ if [ -d /root ]; then
 fi
 
 imgs=(
-  cut-01-tmp_sector-1
-  cut-01-tmc_sector-1
-  cut-02-pm_sector-1
-  cut-03-timc_sector-1
-  cut-04-thr_sector-1
-  cut-05-sf_sector-1
-  cut-05-sfc_sector-1
-  cut-05-sf_sector-all
-  cut-06-ECU_sector-all
-  cut-07-ECV_sector-all
-  cut-08-ECW_sector-all
-  cut-09-UVW_sector-all
-  cut-10-mip_sector-1
-  cut-10-mip_sector-all
-  cut-11-shsh_sector-1
-  cut-12-nphe_sector-all
-  epidsummary
+
 )
 
 imgs_slices=(
-  slice-06_cut-01-ts_sector-1
-  slice-14_cut-01-ts_sector-1
-  slice-03_cut-03-tim_sector-1
-  slice-08_cut-03-tim_sector-1
-  slice-12_cut-03-tim_sector-1
-  slice-15_cut-03-tim_sector-1
-  slice-04_cut-05-sf_sector-1
-  slice-08_cut-05-sf_sector-1
+
 )
 
 for cc in $imgs; do
