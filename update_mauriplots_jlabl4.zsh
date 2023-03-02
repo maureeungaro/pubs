3b20ff34dcc05a6557d4f1c54fa936bf736834ba
maureeungaro <maureeungaro@gmail.com>
2023-03-02 14:53:29 -0500

    updating plots crontab on jlabl4

--- update_mauriplots_jlabl4.zsh
+++ update_mauriplots_jlabl4.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+*/30  *  *  *  * flock -n $HOME/.plots.lock $HOME/update_mauriplots_jlabl4.zsh >& $HOME/logs/plots.log   >/dev/null 2>&1
 
 # This script is meant to be run by a cronjob on jlabl4 to update
 # the mauriPlots repo on /userweb/ungaro/public_html
@@ -7,7 +8,7 @@
 plots=/userweb/ungaro/public_html/plots
 plotsr=https://github.com/mauriPlots
 
-plots_r=(epid ppid vertex)
+plots_r=(epid ppid vertex efid)
 cd $plots
 for r in $plots_r; do
   echo " > updating $r"
