795e516ae74cdecbb8af8dbf7aaa1eb4ea7173b9
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-06-22 10:32:27 -0400

    removed making pdfs from ci, using new script

---
+++ ci/test_pdf.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env zsh
+
+# Purpose:
+# Make the requested pdf
+
+# Container run:
+# docker run -it --rm maureeungaro/base:fedora36-latex sh
+# git clone https://github.com/maureeungaro/pubs       /root/pubs && cd /root/pubs
+# ./ci/make_pdf.sh -d pi0_resonance_regions/pid/electron
+# ./ci/make_pdf.sh -d pi0_resonance_regions
+
+
+Help()
+{
+	# Display Help
+	echo
+	echo "Syntax: tests.sh [-h|d]"
+	echo
+	echo "Options:"
+	echo
+	echo "-h: Print this Help."
+	echo "-d <Doc>: make pdf for <Doc>"
+	echo
+}
+
+if [ $# -eq 0 ]; then
+	Help
+	exit 1
+fi
+
+while getopts ":hd:" option; do
+   case $option in
+      h)
+         Help
+         exit
+         ;;
+      d)
+         doc_to_make=$OPTARG
+         ;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit 1
+         ;;
+   esac
+done
+
+
+DocDirNotExisting() {
+	echo Test Type dir: $example/$doc_to_make not existing
+	Help
+	exit 3
+}
+
+cdir=$(pwd)
+
+echo "Current dir $cdir content:"
+ls -lrt
+
+[[ -v doc_to_make ]] && echo Making $doc_to_make || DocDirNotExisting
+cd $doc_to_make
+
+
+# if doc_to_make is pi0_resonance_regions_note, makeNote
+case $doc_to_make in
+    pi0_resonance_regions_note)
+        ./makeNote
+        ;;
+    *)
+        scons -c
+        scons
+        ;;
+esac
+
+if [ $? -ne 0 ]; then
+	echo Making $doc_to_make pdf failed
+	exit 1
+fi
+
+echo "Current dir $cdir content:"
+ls -lrt
+
+# this was done in the ci, but it would produce a new pdf version for each commit
+#mv *.pdf $cdir/pdfs/
+#
+#if [ $? -ne 0 ]; then
+#	echo Moving pdf failed
+#	exit 1
+#fi
+#
+#scons -c
+#git config --global --add safe.directory /__w/pubs/pubs
+#
+#
+#cd $cdir
+#echo "Current dir $cdir content:"
+#ls -lrt
+#
+## refresh the pdfs that may have been changed by other actions
+#git pull
---
+++ make_pdfs.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+
+yaml_file=.github/workflows/build.yaml
+dirs_to_test=$(grep -A300 "doc:" $yaml_file | grep "          - " | awk '{print $2}')
+cdir=$(pwd)
+
+DocDirNotExisting() {
+	echo Test Type dir: $example/$doc_to_make not existing
+	Help
+	exit 3
+}
+
+for doc_to_make in $=dirs_to_test
+do
+  [[ -v doc_to_make ]] && echo Making $doc_to_make || DocDirNotExisting
+  cd $cdir/$doc_to_make
+  # if doc_to_make is pi0_resonance_regions_note, makeNote
+  case $doc_to_make in
+    pi0_resonance_regions_note)
+      ./makeNote
+      ;;
+    *)
+      scons -c
+      scons
+      ;;
+  esac
+  if [ $? -ne 0 ]; then
+	  echo Making $doc_to_make pdf failed
+	  exit 1
+	else
+	  mv *.pdf $cdir/pdfs/
+	  scons -c
+  fi
+done
