3525d80bb395004f38bbbfe35eb1b6eb5477a6ee
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-06-27 15:58:16 -0400

    introducing tent function and appendix mechanism

--- pi0_resonance_regions_note/fiducial/electron/copy_images.sh
+++ pi0_resonance_regions_note/fiducial/electron/copy_images.sh
@@ -4,8 +4,6 @@ local_dir=efid
 
 orig_dir=/opt/projects/mauriplots/$local_dir
 
-exit
-
 # if we are on the docker container set orig_dir to /root
 if [ -d /root ]; then
   orig_dir=/root/plots/$local_dir
@@ -13,11 +11,17 @@ if [ -d /root ]; then
 fi
 
 imgs=(
-
+plane-DC1_intsector-1
+plane-DC2_intsector-1
+plane-DC3_intsector-1
+plane-SC_intsector-1
 )
 
 imgs_slices=(
-
+slice-05_sector-1_plane-DC3
+slice-06_sector-1_plane-DC3
+slice-07_sector-1_plane-DC3
+slice-08_sector-1_plane-DC3
 )
 
 for cc in $imgs; do
