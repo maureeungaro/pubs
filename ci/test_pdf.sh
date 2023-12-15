3722572b7ebbc7c64191bfa6b32ee84c61d689a7
mungaro <maurizio.ungaro@cnu.edu>
2023-12-15 10:37:11 -0500

    doc test for proceedings

--- ci/test_pdf.sh
+++ ci/test_pdf.sh
@@ -8,7 +8,8 @@
 # git clone https://github.com/maureeungaro/pubs       /root/pubs && cd /root/pubs
 # ./ci/test_pdf.sh -d notes/pi0_resonance_regions/pid/electron
 # ./ci/test_pdf.sh -d notes/pi0_resonance_regions
-
+# ./ci/test_pdf.sh -d papers/pi0_delta
+# ./ci/test_pdf.sh -d proceedings/chep2023
 
 Help()
 {
@@ -46,7 +47,7 @@ done
 
 
 DocDirNotExisting() {
-	echo Test Type dir: $doc_to_make not existing
+	echo Test Type dir: "$doc_to_make" not existing
 	Help
 	exit 3
 }
@@ -56,8 +57,8 @@ cdir=$(pwd)
 echo "Current dir $cdir content:"
 ls -lrt
 
-[[ -v doc_to_make ]] && echo PDFLatex compiling $doc_to_make || DocDirNotExisting
-cd $doc_to_make
+[[ -v doc_to_make ]] && echo PDFLatex compiling "$doc_to_make" || DocDirNotExisting
+cd "$doc_to_make" || exit
 
 
 # if doc_to_make is pi0_resonance_regions_note, makeNote
@@ -75,7 +76,7 @@ case $doc_to_make in
 esac
 
 if [ $exit_code -ne 0 ]; then
-	echo Making $doc_to_make pdf failed
+	echo Making "$doc_to_make" pdf failed
 	cat *.log
 	exit 1
 fi
@@ -85,7 +86,7 @@ echo "Current dir $cdir content:"
 ls -lrt
 echo
 echo "Copying pdf(s) to $cdir/pdfs"
-cp *.pdf $cdir/pdfs/
+cp *.pdf "$cdir"/pdfs/
 if [ $? -ne 0 ]; then
 	echo "Copying pdf(s) failed"
 	exit 1
